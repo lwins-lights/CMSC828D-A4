9c63de3e1f9e3e4a073233a409bb0b273083c656
Yufan Zheng <lwins.lights@gmail.com>
2021-03-02 08:17:28 +0800

    till now: V2.0; TTE = 10.95 + 3.75 = 14.7h

--- server.py
+++ server.py
@@ -33,6 +33,7 @@ app = Flask(__name__)
   fastExe(): Immediately execute an PostgreSQL query
 '''
 def fastExe(conn, comm):
+  debugMsg(comm)
   cur = conn.cursor()
   cur.execute(comm)
   data = cur.fetchall()
@@ -100,6 +101,32 @@ def fetchData(conn, country, xattr, yattr):
     "yearmin":yearmin,
     "yearmax":yearmax}
 
+def fetchDataHistograms(conn, xattr, yattr, year, bins):
+  return {
+    "xdata":fetchDataHistogram(conn, xattr, year, bins),
+    "ydata":fetchDataHistogram(conn, yattr, year, bins)
+  }
+
+def fetchDataHistogram(conn, attr, year, totalBins):
+  year = str(year)
+  mn = fastExe(conn, "SELECT MIN(" + attr + ") FROM nations WHERE year = " + year)[0][0]
+  mx = fastExe(conn, "SELECT MAX(" + attr + ") FROM nations WHERE year = " + year)[0][0]
+  mn = str(mn)
+  mx = str(mx)
+  totalBins = str(totalBins)
+  #debugMsg(totalBins)
+  step = "((" + str(mx) + "-" + str(mn) + ")/" + totalBins + ")"
+  sts = "SELECT *, seq*" + step + "+" + str(mn) + " AS rangeMin, " + \
+    "seq*" + step + "+" + str(mn) + "+" + step + " AS rangeMax " + \
+    "FROM (SELECT CASE WHEN " + attr + "=" + str(mx) + " THEN " + str(int(totalBins) - 1) + " ELSE " + \
+    "FLOOR((" + attr + "-" + str(mn) + ")/" + step + ") END AS seq, " + \
+    "COUNT(*) FROM nations " + \
+    "WHERE (" + attr + ">=" + mn + ") AND (" + attr + "<=" + mx + ") AND (year = " + year + \
+    ") GROUP BY seq ORDER BY seq) t1"
+  return {"data":fastExe(conn, sts), "mn":mn, "mx":mx, "maxcount":fastExe(conn,
+    "SELECT MAX(count) FROM (" + sts + ") t2"
+  )[0][0]}
+
 '''
   init(): Initialization of the server
 '''
@@ -131,11 +158,19 @@ def renderPage():
     .yattr := attribute 2
   .qtype == init:
     NONE
+  .qtype == data_his:
+    .xattr := attribute 1
+    .yattr := attribute 2
+    .year := year
+    .bins := #bins
   ---------- OUTPUT ----------
   .qtype == data:
     .xygraph := data of the line graph: array of tuple [x, y, year]
   .qtype == init:
-    .countrylist := array of tuple like ["China", "CHN"]
+    .countrylist := array of tuple like ["China", "CHN", "Asia", "Middle Income"]
+  .qtype == data_his:
+    .xdata = data of the histogram
+    .ydata 
 '''
 @app.route('/get-data')
 def getData():
@@ -147,6 +182,12 @@ def getData():
     data = fetchData(conn, country, xattr, yattr)
   if qtype == 'init':
     data = fetchInit(conn)
+  if qtype == 'data_his':
+    xattr = request.args.get('xattr')
+    yattr = request.args.get('yattr')
+    year = request.args.get('year')
+    bins = request.args.get('bins')
+    data = fetchDataHistograms(conn, xattr, yattr, year, bins)
   resp = Response(response=json.dumps(data),status=200, mimetype='application/json')
   h = resp.headers
   h['Access-Control-Allow-Origin'] = "*"
@@ -161,6 +202,11 @@ def favicon():
 def getD3js():
   return send_from_directory(os.path.join(app.root_path, 'static'),
     'd3.v6.min.js',mimetype='text/javascript')
+
+@app.route('/d3-simple-slider.min.js')
+def getD3Sliderjs():
+  return send_from_directory(os.path.join(app.root_path, 'static'),
+    'd3-simple-slider.min.js',mimetype='text/javascript')
 
 '''
   Main part
