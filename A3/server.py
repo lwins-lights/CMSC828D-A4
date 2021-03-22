cd442d1aabb39f61bcff8a01c3f4c45f78dba1da
Yufan Zheng <lwins.lights@gmail.com>
2021-03-23 07:02:59 +0800

    writeup

--- A3/server.py
+++ A3/server.py
@@ -54,7 +54,7 @@ app = Flask(__name__)
 ###############################################################################
 
 def fastExe(conn, comm):
-  debugMsg(comm)
+  #debugMsg(comm)
   cur = conn.cursor()
   cur.execute(comm)
   try:
